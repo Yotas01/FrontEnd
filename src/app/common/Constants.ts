4a15ec7d69c7a56b7129638c1b49e2e754b8dcde
Yotas01 <dani.monsalveg@gmail.com>
2022-06-15 14:40:00 -0500

    Minor fix

--- src/app/common/Constants.ts
+++ src/app/common/Constants.ts
@@ -1,5 +1,6 @@
 import { Section } from "../model/section/section";
 
 export class Constants{
+    public static baseUrl:string = "http://localhost:8090";
     public static sectionBase = new Section(1,1,"",1,1,new Map);
 }
