9a3bd728c54459d24196e89e7b37c45d729a8647
Yotas01 <dani.monsalveg@gmail.com>
2022-06-15 14:37:50 -0500

    Back now runs in port 8090

--- src/app/common/Constants.ts
+++ src/app/common/Constants.ts
@@ -1,6 +1,5 @@
 import { Section } from "../model/section/section";
 
 export class Constants{
-    public static baseUrl:string = "http://localhost:80";
     public static sectionBase = new Section(1,1,"",1,1,new Map);
 }
